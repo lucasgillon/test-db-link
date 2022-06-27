9f117dbfa004dcd1daf93e96e2be2440cf429c24
lucasgillon <106580814+lucasgillon@users.noreply.github.com>
2022-06-27 13:35:45 +0200

    initial commit

---
+++ src/connectionDB/ConnectPostgreSQL.java
@@ -0,0 +1,24 @@
+package connectionDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ConnectPostgreSQL {
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5050/",
+                    System.getenv("db-user"), System.getenv("db-password"));
+            Statement stmt = conn.createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM person");
+            while (res.next()) System.out.println("name: " + res.getString(1) + ",  " +
+                    "age: " + res.getInt(2) + ",  " +
+                    "verified: " + res.getBoolean(3));
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
