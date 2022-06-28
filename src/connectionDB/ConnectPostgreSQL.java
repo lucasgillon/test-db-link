24e9444079b9be208e210dd9511f2bb731eed75f
lucasgillon <lucas.gillon@adeo.com>
2022-06-28 11:18:33 +0200

    change try/catch to single section with throws for main class

--- src/connectionDB/ConnectPostgreSQL.java
+++ src/connectionDB/ConnectPostgreSQL.java
@@ -1,22 +1,15 @@
 package connectionDB;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class ConnectPostgreSQL {
-    public static void main(String[] args) {
-        try {
-            Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5050/", System.getenv("db-user"), System.getenv("db-password"));
-            Statement stmt = conn.createStatement();
-            ResultSet res = stmt.executeQuery("SELECT * FROM product");
-            while (res.next())
-                System.out.println("name: " + res.getString(1) + ",  " + "age: " + res.getInt(2) + ",  " + "verified: " + res.getBoolean(3));
-            conn.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5050/", System.getenv("db-user"), System.getenv("db-password"));
+        Statement stmt = conn.createStatement();
+        ResultSet res = stmt.executeQuery("SELECT * FROM person");
+        while (res.next())
+            System.out.println("name: " + res.getString(1) + ",  " + "age: " + res.getInt(2) + ",  " + "verified: " + res.getBoolean(3));
+        conn.close();
     }
 }
